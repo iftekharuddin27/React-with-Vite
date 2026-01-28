d2e1586d9e6924684010d89b36b5b9ca5ecbbc0c
iftekharuddin27 <uddiniftekhar84@gmail.com>
2026-01-28 22:27:14 +0600

    Update

--- 03counterproject/src/App.jsx
+++ 03counterproject/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Card from './components/cards.jsx'
 
@@ -10,10 +8,9 @@ function App() {
   return (
     <>
     <h1 className='mb-5'>Cards for sale</h1>
-    <Card username="Mr. Monkey" Price="2.00 $" serial='#277'/>
-    <br /><br />
+    <Card username="Mr. Monkey" Price="2.00 $" serial='#277'/> <br />
     <Card username='Lufy D Monkey' Price="5.00 $" serial='#278'/>
-    </>
+ </>
   )
 }
 
